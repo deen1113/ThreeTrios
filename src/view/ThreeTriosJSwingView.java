7cad3e18f09212b02f4c8ca82959e2dbfac9aaf1
deen1113 <khan.de@northeastern.edu>
2024-11-10 16:06:28 -0500

    fixed style

--- src/view/ThreeTriosJSwingView.java
+++ src/view/ThreeTriosJSwingView.java
@@ -1,7 +1,7 @@
 package view;
 
-import java.awt.*;
-
+import java.awt.Color;
+import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -9,22 +9,18 @@ import model.IReadonlyThreeTriosModel;
 
 public class ThreeTriosJSwingView extends JFrame {
   IReadonlyThreeTriosModel model;
-  private JPanel redSide;
-  private JPanel blueSide;
-  private JPanel grid;
-
 
 
   public ThreeTriosJSwingView(IReadonlyThreeTriosModel model) {
     this.model = model;
 
-    redSide = new HandView(Color.PINK, model.getRedHand());
+    JPanel redSide = new HandView(Color.PINK, model.getRedHand());
     add(redSide, BorderLayout.WEST);
 
-    blueSide = new HandView(Color.CYAN, model.getBlueHand());
+    JPanel blueSide = new HandView(Color.CYAN, model.getBlueHand());
     add(blueSide, BorderLayout.EAST);
 
-    grid = new GridView(model);
+    JPanel grid = new GridView(model);
     add(grid, BorderLayout.CENTER);
   }
 }
